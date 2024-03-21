56ce960b9441bd4d14669b6880f348c4a12a325e
Cjp103 <122712478+Cjp103@users.noreply.github.com>
2024-03-21 12:24:14 -0400

    Update mainGameCode.ino
    fix errors

--- mainGameCode.ino
+++ mainGameCode.ino
@@ -1,14 +1,97 @@
-/*
-    MPU6050 Triple Axis Gyroscope & Accelerometer. Simple Gyroscope Example.
-    Read more: http://www.jarzebski.pl/arduino/czujniki-i-sensory/3-osiowy-zyroskop-i-akcelerometr-mpu6050.html
-    GIT: https://github.com/jarzebski/Arduino-MPU6050
-    Web: http://www.jarzebski.pl
-    (c) 2014 by Korneliusz Jarzebski
-*/
+// Kappa Bop-it Project
 
-#include <Wire.h>
+//define pitches
+#define NOTE_B0 31
+#define NOTE_C1 33
+#define NOTE_CS1 35
+#define NOTE_D1 37
+#define NOTE_DS1 39
+#define NOTE_E1 41
+#define NOTE_F1 44
+#define NOTE_FS1 46
+#define NOTE_G1 49
+#define NOTE_GS1 52
+#define NOTE_A1 55
+#define NOTE_AS1 58
+#define NOTE_B1 62
+#define NOTE_C2 65
+#define NOTE_CS2 69
+#define NOTE_D2 73
+#define NOTE_DS2 78
+#define NOTE_E2 82
+#define NOTE_F2 87
+#define NOTE_FS2 93
+#define NOTE_G2 98
+#define NOTE_GS2 104
+#define NOTE_A2 110
+#define NOTE_AS2 117
+#define NOTE_B2 123
+#define NOTE_C3 131
+#define NOTE_CS3 139
+#define NOTE_D3 147
+#define NOTE_DS3 156
+#define NOTE_E3 165
+#define NOTE_F3 175
+#define NOTE_FS3 185
+#define NOTE_G3 196
+#define NOTE_GS3 208
+#define NOTE_A3 220
+#define NOTE_AS3 233
+#define NOTE_B3 247
+#define NOTE_C4 262
+#define NOTE_CS4 277
+#define NOTE_D4 294
+#define NOTE_DS4 311
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_FS4 370
+#define NOTE_G4 392
+#define NOTE_GS4 415
+#define NOTE_A4 440
+#define NOTE_AS4 466
+#define NOTE_B4 494
+#define NOTE_C5 523
+#define NOTE_CS5 554
+#define NOTE_D5 587
+#define NOTE_DS5 622
+#define NOTE_E5 659
+#define NOTE_F5 698
+#define NOTE_FS5 740
+#define NOTE_G5 784
+#define NOTE_GS5 831
+#define NOTE_A5 880
+#define NOTE_AS5 932
+#define NOTE_B5 988
+#define NOTE_C6 1047
+#define NOTE_CS6 1109
+#define NOTE_D6 1175
+#define NOTE_DS6 1245
+#define NOTE_E6 1319
+#define NOTE_F6 1397
+#define NOTE_FS6 1480
+#define NOTE_G6 1568
+#define NOTE_GS6 1661
+#define NOTE_A6 1760
+#define NOTE_AS6 1865
+#define NOTE_B6 1976
+#define NOTE_C7 2093
+#define NOTE_CS7 2217
+#define NOTE_D7 2349
+#define NOTE_DS7 2489
+#define NOTE_E7 2637
+#define NOTE_F7 2794
+#define NOTE_FS7 2960
+#define NOTE_G7 3136
+#define NOTE_GS7 3322
+#define NOTE_A7 3520
+#define NOTE_AS7 3729
+#define NOTE_B7 3951
+#define NOTE_C8 4186
+#define NOTE_CS8 4435
+#define NOTE_D8 4699
+#define NOTE_DS8 4978
+
 #include <MPU6050.h>
-#include <pitches.h>
 
 MPU6050 mpu;
 
@@ -22,7 +105,7 @@ int themeDurations[] = {
   8, 8, 8, 8, 3, 3, 4, 2, 2
 };  // note durations: 2 = half note, 4 = quarter note, 8 = eighth note, etc.
 
-int themeSize = sizeof(melody1) / sizeof(melody1[0]);
+int themeSize = sizeof(theme) / sizeof(theme[0]);
 
 // ----------------------------------------------------------------------------------
 
@@ -35,7 +118,7 @@ int minorThemeDurations[] = {
   8, 8, 8, 8, 3, 3, 4, 2, 2
 };  // note durations: 2 = half note, 4 = quarter note, 8 = eighth note, etc.
 
-int minorThemeSize = sizeof(melody1) / sizeof(melody1[0]);
+int minorThemeSize = sizeof(minorTheme) / sizeof(minorTheme[0]);
 
 // ----------------------------------------------------------------------------------
 
@@ -49,7 +132,7 @@ int forehandDurations[] = {
   4
 };  // note durations: 2 = half note, 4 = quarter note, 8 = eighth note, etc.
 
-int forehandSize = sizeof(melody1) / sizeof(melody1[0]);
+int forehandSize = sizeof(forehand) / sizeof(forehand[0]);
 
 // ----------------------------------------------------------------------------------
 // Backhand
@@ -61,7 +144,7 @@ int backhandDurations[] = {
   4, 4
 };
 
-int backhandSize = sizeof(melody2) / sizeof(melody2[0]);
+int backhandSize = sizeof(backhand) / sizeof(backhand[0]);
 
 // ----------------------------------------------------------------------------------
 // Serve
@@ -73,7 +156,7 @@ int serveDurations[] = {
   4, 4, 4
 };
 
-int serveSize = sizeof(melody3) / sizeof(melody3[0]);
+int serveSize = sizeof(serve) / sizeof(serve[0]);
 
 // ----------------------------------------------------------------------------------
 
@@ -86,6 +169,14 @@ int z = 0;
 int bScore[6];
 int score = 0;
 int gameDelay = 51000;
+
+//hex pins
+int A = 4;
+int B = 3; 
+int C = 2; 
+int D = 5; 
+int E = 9; 
+int F = 10;
 
 // ----------------------------------------------------------------------------------
 // -----------------------------------Game Code--------------------------------------
@@ -251,7 +342,7 @@ void loop() {
           // stop the tone playing at the output pin
           noTone(8);
         }
-        delay(10000)
+        delay(10000);
         exit(-1); 
     }
 
@@ -349,5 +440,3 @@ void hexDisplay(int score){
    digitalWrite(F, TOTAL[0]);   // 2's   (tens)
 		    
 }
-
-
